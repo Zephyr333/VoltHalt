78673416f35267973ef42bd75842d5200303f923
Ananthu <whoami.ananthu@gmail.com>
2026-08-09 10:31:36 +0530

    Delete app/src/main/java/com/im_atp/volthalt/UpdateChecker.kt

--- app/src/main/java/com/im_atp/volthalt/UpdateChecker.kt
+++
@@ -1,4 +0,0 @@
-package com.im_atp.volthalt
-
-// UpdateChecker was removed. The app is fully offline and self-contained.
-// APK sharing is handled directly via the share button on the main screen.
